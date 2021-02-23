863652331029229c252f4c567ad3c9f95baa2058
SeongminHa <seongminha0910@gmail.com>
2021-02-24 02:36:40 +0900

    react-router에서 회원가입 화면 구현

--- react-app/src/shared/App.js
+++ react-app/src/shared/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Register } from '../pages';
 
 class App extends Component {
     render() {
         return (
             <div>
-                Hello React-Router
+                <Route exact path="/" component={Register}/>
             </div>
         );
     }
