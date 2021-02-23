d472121b9e4f5add4b56d97b4e1ed404d066c088
SeongminHa <seongminha0910@gmail.com>
2021-02-24 04:00:24 +0900

    handleChangeEvent 함수 구현

--- react-app/src/pages/Register.js
+++ react-app/src/pages/Register.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Register(){
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    function handleChangeEmail(e){
+        setEmail(e.target.value);
+    }
+
+    function handleChangePassword(e){
+        setPassword(e.target.value);
+    }
 
     return (
         <>
             <h2>Register</h2>
             <form>
                 Email Address<br/>
-                <input type="text" placeholder="Enter Email Address" /><br/>
+                <input type="text" onChange={handleChangeEmail} placeholder="Enter Email Address" /><br/>
                 Password<br/>
-                <input type="password" placeholder="Enter Password" /><br/>
+                <input type="password" onChange={handleChangePassword} placeholder="Enter Password" /><br/>
                 <button type="submit">가입하기</button>
             </form>
         </>
