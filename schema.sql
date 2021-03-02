f5476c182b6642df4e22e0e95025827692cfe7fe
SeongminHa <seongminha0910@gmail.com>
2021-03-03 07:03:31 +0900

    DB SQL 수정

--- schema.sql
+++ schema.sql
@@ -1,68 +1,88 @@
+-- -----------------------------------------------------
+-- Schema skidmarker
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `skidmarker` DEFAULT CHARACTER SET utf8 ;
+USE `skidmarker` ;
+
+-- -----------------------------------------------------
+-- Table `skidmarker`.`user`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `skidmarker`.`user` (
-  `user_id` INT NOT NULL AUTO_INCREMENT,
-  `user_email` VARCHAR(65) NOT NULL,
-  `user_password` VARCHAR(128) NOT NULL,
-  `user_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE INDEX `user_email_UNIQUE` (`user_email` ASC))
-ENGINE = InnoDB
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `user_email_UNIQUE` (`email` ASC));
 
-CREATE TABLE IF NOT EXISTS `skidmarker`.`certificate` (
-  `certificate_id` INT NOT NULL AUTO_INCREMENT,
-  `certificate_title` VARCHAR(45) NOT NULL,
-  `certificate_detail` VARCHAR(256) NOT NULL,
-  `certificate_date` DATETIME NOT NULL,
-  `user_user_id` INT NOT NULL,
-  PRIMARY KEY (`certificate_id`),
-  INDEX `fk_certificate_user_idx` (`user_user_id` ASC),
-  CONSTRAINT `fk_certificate_user`
-    FOREIGN KEY (`user_user_id`)
-    REFERENCES `skidmarker`.`user` (`user_id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
 
-CREATE TABLE IF NOT EXISTS `skidmarker`.`competition` (
-  `competition_id` INT NOT NULL AUTO_INCREMENT,
-  `competition_title` VARCHAR(45) NOT NULL,
-  `competition_detail` VARCHAR(256) NOT NULL,
-  `user_user_id` INT NOT NULL,
-  PRIMARY KEY (`competition_id`),
-  INDEX `fk_competition_user1_idx` (`user_user_id` ASC),
-  CONSTRAINT `fk_competition_user1`
-    FOREIGN KEY (`user_user_id`)
-    REFERENCES `skidmarker`.`user` (`user_id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-
-CREATE TABLE IF NOT EXISTS `skidmarker`.`project` (
-  `project_id` INT NOT NULL AUTO_INCREMENT,
-  `project_title` VARCHAR(45) NOT NULL,
-  `project_detail` VARCHAR(256) NOT NULL,
-  `project_start` DATETIME NOT NULL,
-  `project_end` DATETIME NOT NULL,
-  `user_user_id` INT NOT NULL,
-  PRIMARY KEY (`project_id`),
-  INDEX `fk_project_user1_idx` (`user_user_id` ASC),
-  CONSTRAINT `fk_project_user1`
-    FOREIGN KEY (`user_user_id`)
-    REFERENCES `skidmarker`.`user` (`user_id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-
+-- -----------------------------------------------------
+-- Table `skidmarker`.`education`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `skidmarker`.`education` (
-  `education_id` INT NOT NULL AUTO_INCREMENT,
-  `school_name` VARCHAR(45) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `univ_name` VARCHAR(45) NOT NULL,
   `major` VARCHAR(45) NOT NULL,
   `degree` VARCHAR(45) NOT NULL,
-  `user_user_id` INT NOT NULL,
-  PRIMARY KEY (`education_id`),
-  INDEX `fk_education_user1_idx` (`user_user_id` ASC),
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_education_user1_idx` (`user_id` ASC),
   CONSTRAINT `fk_education_user1`
-    FOREIGN KEY (`user_user_id`)
-    REFERENCES `skidmarker`.`user` (`user_id`)
+    FOREIGN KEY (`user_id`)
+    REFERENCES `skidmarker`.`user` (`id`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
+    ON UPDATE CASCADE);
+
+
+-- -----------------------------------------------------
+-- Table `skidmarker`.`award`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `skidmarker`.`award` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `detail` VARCHAR(255) NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_competition_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_competition_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `skidmarker`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
+
+
+-- -----------------------------------------------------
+-- Table `skidmarker`.`project`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `skidmarker`.`project` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `detail` VARCHAR(255) NOT NULL,
+  `start_date` DATETIME NOT NULL,
+  `end_date` DATETIME NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_project_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_project_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `skidmarker`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
+
+
+-- -----------------------------------------------------
+-- Table `skidmarker`.`certificate`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `skidmarker`.`certificate` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `detail` VARCHAR(255) NOT NULL,
+  `acq_date` DATETIME NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_certificate_user_idx` (`user_id` ASC),
+  CONSTRAINT `fk_certificate_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `skidmarker`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
