00f92efcc76608f9a9d9ca80465c7ece1b941afd
SeongminHa <seongminha0910@gmail.com>
2021-03-07 01:43:41 +0900

    네트워크 href 활성화

--- react-app/src/pages/User.js
+++ react-app/src/pages/User.js
@@ -184,7 +184,7 @@ function User() {
                             <Nav.Link href="/user">메인</Nav.Link>
                             </Nav.Item>
                             <Nav.Item>
-                            <Nav.Link eventKey="/network">네트워크</Nav.Link>
+                            <Nav.Link href="/network">네트워크</Nav.Link>
                             </Nav.Item>
                             <Nav.Item>
                             { sessionStorage.length !== 0 ? 
