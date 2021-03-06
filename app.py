f4ff382ccb8132e50defc981f403e1fd2f7cdeea
SeongminHa <seongminha0910@gmail.com>
2021-03-06 23:38:31 +0900

    NETWORK API 구현

--- app.py
+++ app.py
@@ -320,5 +320,17 @@ def delete():
 
     return jsonify(status = "delete success")
 
+# 사용자 네트워크 API
+
+@app.route('/network', methods=['GET'])
+def readAll():
+    
+    sql = "SELECT `id`, `name`, `email` FROM `user`;
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    db.commit()
+
+    return jsonify(status = "success", result)
+
 if __name__ == '__main__':
     app.run("0.0.0.0", port=5000, threaded = False)
