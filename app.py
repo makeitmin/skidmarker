3966e49db3857be1ab4b1b47ce3cf07e3cc03392
SeongminHa <seongminha0910@gmail.com>
2021-03-06 23:56:39 +0900

    NETWORK API 오타 수정

--- app.py
+++ app.py
@@ -333,7 +333,7 @@ def readAll():
     result = cursor.fetchall()
     db.commit()
 
-    return jsonify(status = "success", result)
+    return jsonify(status = "success", result = result)
 
 if __name__ == '__main__':
     app.run("0.0.0.0", port=5000, threaded = False)
