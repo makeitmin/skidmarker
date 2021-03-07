dbf4cc8db6dd3e27350dbff756e0589d2d053996
SeongminHa <seongminha0910@gmail.com>
2021-03-07 14:15:26 +0900

    OtherUser Component 라우팅

--- react-app/src/pages/index.js
+++ react-app/src/pages/index.js
@@ -2,6 +2,7 @@ export { default as Login } from './Login';
 export { default as Register } from './Register';
 export { default as User } from './User';
 export { default as Network } from './Network';
+export { default as OtherUser } from './OtherUser';
 export { default as Education } from '../components/user/Education';
 export { default as Award } from '../components/user/Award';
 export { default as Project } from '../components/user/Project';
